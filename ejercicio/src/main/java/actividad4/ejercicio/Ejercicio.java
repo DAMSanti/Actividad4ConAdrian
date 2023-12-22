751ed229f7bdae58b37132c1f1fc64ecce3a4f0a
PipasConSal <adrian.borregocaceres@gmail.com>
2023-12-22 16:06:02 +0100

    ejemplo actividad 4

---
+++ ejercicio/src/main/java/actividad4/ejercicio/Ejercicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package actividad4.ejercicio;
+
+/**
+ *
+ * @author DAM127
+ */
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Santi");
+    }
+}
