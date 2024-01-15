8662ea40db2e6b22dcd8569ffacf6bdb2ab1e0d3
DAMSanti <santiagomanuel.tamayoarozamena@iesmiguelherrero.com>
2024-01-15 20:14:20 +0100

    .

---
+++ infinito/src/main/java/unidad4/infinito/Infinito.java
@@ -0,0 +1,13 @@
+package unidad4.infinito;
+
+/**
+ *
+ * @author DAM104
+ */
+public class Infinito {
+
+    public static void main(String[] args) {
+        int numero = 24 / 0;
+        System.out.println(numero);
+    }
+}
